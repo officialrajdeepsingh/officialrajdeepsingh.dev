ed752875dbb0ef4c32d20c91c036dcd809f86a2e
officialrajdeepsingh <radewala.2@gmail.com>
2023-07-04 16:49:49 +0530

    enabling staticImage

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-
 const withNextra = require("nextra")({
   theme: "section-theme-blog",
   themeConfig: "./theme.config.tsx",
+  staticImage: true,
 });
- 
+
 module.exports = withNextra({
   reactStrictMode: true,
 });
