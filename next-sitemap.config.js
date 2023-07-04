c8d4d1afbbcd812f6244256a303787e5752b93df
officialrajdeepsingh <radewala.2@gmail.com>
2023-07-04 18:04:50 +0530

    adding production domain\

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://example.com",
+  siteUrl: process.env.SITE_URL || "https://officialblog.vercel.app",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
