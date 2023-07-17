ef4202dfc01d4718f7d3a63445656078409e7b20
officialrajdeepsingh <radewala.2@gmail.com>
2023-07-17 18:43:51 +0530

    update the url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://officialblog.vercel.app",
+  siteUrl: process.env.SITE_URL || "https://officialrajdeepsingh.dev",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
