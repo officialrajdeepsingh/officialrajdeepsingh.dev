e8f05d804cb490431667c8b66501b1d9e70a15e6
officialrajdeepsingh <radewala.2@gmail.com>
2023-07-18 19:27:32 +0530

    adding google ads script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+         <Script strategy="lazyOnload"
+            crossOrigin="anonymous" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9978927393009784" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
