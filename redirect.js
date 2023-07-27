e2ec21513bb172c213956b004ca07d76e82c9910
officialrajdeepsingh <radewala.2@gmail.com>
2023-07-27 20:52:58 +0530

    list of redirect url

---
+++ redirect.js
@@ -0,0 +1,492 @@
+const redirectUrl = [
+  {
+    "source": "/5-best-javascript-library-for-you",
+    "destination": "/posts/5-best-javascript-library-for-you",
+    "permanent": true
+  },
+  {
+    "source": "/80-port-show-at-the-end-of-your-ghost-domain-name",
+    "destination": "/posts/80-port-show-at-the-end-of-your-ghost-domain-name",
+    "permanent": true
+  },
+  {
+    "source": "/best-way-to-install-zoom-in-your-ubuntu",
+    "destination": "/posts/best-way-to-install-zoom-in-your-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/body-tag-in-html-5",
+    "destination": "/posts/body-tag-in-html-5",
+    "permanent": true
+  },
+  {
+    "source": "/create-a-reactjs-app-with-pnpm",
+    "destination": "/posts/create-a-reactjs-app-with-pnpm",
+    "permanent": true
+  },
+  {
+    "source": "/create-modern-quiz-with-react-js",
+    "destination": "/posts/create-modern-quiz-with-react-js",
+    "permanent": true
+  },
+  {
+    "source": "/disable-page-speed-in-bitnami-ghost-cms",
+    "destination": "/posts/disable-page-speed-in-bitnami-ghost-cms",
+    "permanent": true
+  },
+  {
+    "source": "/doctypes-in-html",
+    "destination": "/posts/doctypes-in-html",
+    "permanent": true
+  },
+  {
+    "source": "/featured-image-does-not-load-after-installing-ssl-in-bitnami",
+    "destination": "/posts/featured-image-does-not-load-after-installing-ssl-in-bitnami",
+    "permanent": true
+  },
+  {
+    "source": "/head-tag-in-html",
+    "destination": "/posts/head-tag-in-html",
+    "permanent": true
+  },
+  {
+    "source": "/heading-tags-in-html5",
+    "destination": "/posts/heading-tags-in-html5",
+    "permanent": true
+  },
+  {
+    "source": "/how-do-you-update-your-project-s-old-npm-package-with-a-single-npm-command",
+    "destination": "/posts/how-do-you-update-your-project-s-old-npm-package-with-a-single-npm-command",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-add-ads-txt-file-in-my-ghost-cms",
+    "destination": "/posts/how-to-add-ads-txt-file-in-my-ghost-cms",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-add-css-in-next-js",
+    "destination": "/posts/how-to-add-css-in-next-js",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-add-custom-context-menu-in-react-js",
+    "destination": "/posts/how-to-add-custom-context-menu-in-react-js",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-add-custom-domain-on-your-google-compute-engine",
+    "destination": "/posts/how-to-add-custom-domain-on-your-google-compute-engine",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-add-search-bar-functionality-in-ghost-cms-help-of-google-custom-search",
+    "destination": "/posts/how-to-add-search-bar-functionality-in-ghost-cms-help-of-google-custom-search",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-capture-screenshots-in-raspberry-pi-4",
+    "destination": "/posts/how-to-capture-screenshots-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-check-the-snap-store-package-available-for-raspberry-pi-4-or-not",
+    "destination": "/posts/how-to-check-the-snap-store-package-available-for-raspberry-pi-4-or-not",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-config-gmail-smtp-in-ghost-cms",
+    "destination": "/posts/how-to-config-gmail-smtp-in-ghost-cms",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-configure-own-domain-name-for-a-ghost-blog-on-compute-engine",
+    "destination": "/posts/how-to-configure-own-domain-name-for-a-ghost-blog-on-compute-engine",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-configure-ssl-certificate-in-ghost-cms-with-bitnami-on-google-cloud-platform",
+    "destination": "/posts/how-to-configure-ssl-certificate-in-ghost-cms-with-bitnami-on-google-cloud-platform",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-convert-a-video-or-image-into-a-gif",
+    "destination": "/posts/how-to-convert-a-video-or-image-into-a-gif",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-create-account-on-google-cloud-platform-and-verify-billing-account",
+    "destination": "/posts/how-to-create-account-on-google-cloud-platform-and-verify-billing-account",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-create-web-stories-on-ghost-cms",
+    "destination": "/posts/how-to-create-web-stories-on-ghost-cms",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-generate-a-yarn-lock-file-based-on-a-package-json-file-for-npm-or-pnpm",
+    "destination": "/posts/how-to-generate-a-yarn-lock-file-based-on-a-package-json-file-for-npm-or-pnpm",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-brave-browser-in-ubuntu-22.04",
+    "destination": "/posts/how-to-install-brave-browser-in-ubuntu-22.04",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-bun-in-ubuntu",
+    "destination": "/posts/how-to-install-bun-in-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-chromium-in-ubuntu",
+    "destination": "/posts/how-to-install-chromium-in-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-deno-in-linux",
+    "destination": "/posts/how-to-install-deno-in-linux",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-filezilla-in-linux",
+    "destination": "/posts/how-to-install-filezilla-in-linux",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-filezilla-in-raspberry-pi-4",
+    "destination": "/posts/how-to-install-filezilla-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-flatpak-in-ubuntu-22.04",
+    "destination": "/posts/how-to-install-flatpak-in-ubuntu-22.04",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-go-language-in-raspberry-pi-4-and-ubuntu",
+    "destination": "/posts/how-to-install-go-language-in-raspberry-pi-4-and-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-neovim-ide-in-raspberry-pi-4-and-ubuntu",
+    "destination": "/posts/how-to-install-neovim-ide-in-raspberry-pi-4-and-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-pycharm-in-raspi-4",
+    "destination": "/posts/how-to-install-pycharm-in-raspi-4",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-rust-language-in-raspberry-pi-4-and-ubuntu",
+    "destination": "/posts/how-to-install-rust-language-in-raspberry-pi-4-and-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-snap-command-in-raspberry-pi-4",
+    "destination": "/posts/how-to-install-snap-command-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-sublime-text-ide-in-raspberry-pi-4",
+    "destination": "/posts/how-to-install-sublime-text-ide-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-sublime-text-ide-in-ubuntu",
+    "destination": "/posts/how-to-install-sublime-text-ide-in-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-install-the-pnpm-package-manger-in-ubuntu",
+    "destination": "/posts/how-to-install-the-pnpm-package-manger-in-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-record-sound-in-ubuntu",
+    "destination": "/posts/how-to-record-sound-in-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-remove-bitnami-corner-banner-from-ghost-cms-on-google-cloud-platform",
+    "destination": "/posts/how-to-remove-bitnami-corner-banner-from-ghost-cms-on-google-cloud-platform",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-remove-unnecessary-software-from-your-raspberry-pi-4",
+    "destination": "/posts/how-to-remove-unnecessary-software-from-your-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-rename-multiple-files-in-vscode-visual-studio-code",
+    "destination": "/posts/how-to-rename-multiple-files-in-vscode-visual-studio-code",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-run-ghost-on-google-cloud-with-bitnami",
+    "destination": "/posts/how-to-run-ghost-on-google-cloud-with-bitnami",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-update-ghost-cms-in-google-cloud-deploy-with-bitnami",
+    "destination": "/posts/how-to-update-ghost-cms-in-google-cloud-deploy-with-bitnami",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-update-pnpm-version",
+    "destination": "/posts/how-to-update-pnpm-version",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-update-the-node-js-version-in-windows",
+    "destination": "/posts/how-to-update-the-node-js-version-in-windows",
+    "permanent": true
+  },
+  {
+    "source": "/how-to-use-bootstrap-in-react-js",
+    "destination": "/posts/how-to-use-bootstrap-in-react-js",
+    "permanent": true
+  },
+  {
+    "source": "/hr-tag-in-html5",
+    "destination": "/posts/hr-tag-in-html5",
+    "permanent": true
+  },
+  {
+    "source": "/html-version-history",
+    "destination": "/posts/html-version-history",
+    "permanent": true
+  },
+  {
+    "source": "/install-android-studio-in-raspberry-pi-4",
+    "destination": "/posts/install-android-studio-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/install-firefox-latest-version-in-raspberry-pi-4",
+    "destination": "/posts/install-firefox-latest-version-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/install-node-js-and-npm-latest-version-on-raspberry-pi-4",
+    "destination": "/posts/install-node-js-and-npm-latest-version-on-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/install-vlc-media-player-in-raspberry-pi-4",
+    "destination": "/posts/install-vlc-media-player-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/install-vlc-media-player-in-ubuntu",
+    "destination": "/posts/install-vlc-media-player-in-ubuntu",
+    "permanent": true
+  },
+  {
+    "source": "/install-vscode-editor-in-raspberry-pi-4",
+    "destination": "/posts/install-vscode-editor-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/is-deno-available-for-raspberry-pi-4",
+    "destination": "/posts/is-deno-available-for-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/it-is-possible-to-create-a-mobile-application-in-raspberry-pi-4",
+    "destination": "/posts/it-is-possible-to-create-a-mobile-application-in-raspberry-pi-4",
+    "permanent": true
+  },
+  {
+    "source": "/keyboard-shortcut-keys-for-linux-terminal",
+    "destination": "/posts/keyboard-shortcut-keys-for-linux-terminal",
+    "permanent": true
+  },
+  {
+    "source": "/learn-react-js-with-youtube-tutorials",
+    "destination": "/posts/learn-react-js-with-youtube-tutorials",
+    "permanent": true
+  },
+  {
+    "source": "/linux-basic-introduction",
+    "destination": "/posts/linux-basic-introduction",
+    "permanent": true
+  },
+  {
+    "source": "/ls-command-in-linux",
+    "destination": "/posts/ls-command-in-linux",
+    "permanent": true
+  },
+  {
+    "source": "/mkdir-command-in-linux",
+    "destination": "/posts/mkdir-command-in-linux",
+    "permanent": true
+  },
+  {
+    "source": "/new-feature-in-html-5-with-examples",
+    "destination": "/posts/new-feature-in-html-5-with-examples",
+    "permanent": true
+  },
+  {
+    "source": "/npm-list-command",
+    "destination": "/posts/npm-list-command",
+    "permanent": true
+  },
+  {
+    "source": "/npm-outdated-command",
+    "destination": "/posts/npm-outdated-command",
+    "permanent": true
+  },
+  {
+    "source": "/npm-repo-command",
+    "destination": "/posts/npm-repo-command",
+    "permanent": true
+  },
+  {
+    "source": "/paragraph-tag-in-html",
+    "destination": "/posts/paragraph-tag-in-html",
+    "permanent": true
+  },
+  {
+    "source": "/pwd-command-in-linux",
+    "destination": "/posts/pwd-command-in-linux",
+    "permanent": true
+  },
+  {
+    "source": "/read-locally-json-file-use-fetch-method-in-javascript",
+    "destination": "/posts/read-locally-json-file-use-fetch-method-in-javascript",
+    "permanent": true
+  },
+  {
+    "source": "/read-locally-json-file-use-fetch-method-in-react-js",
+    "destination": "/posts/read-locally-json-file-use-fetch-method-in-react-js",
+    "permanent": true
+  },
+  {
+    "source": "/read-locally-txt-file-use-fetch-method-in-javascript",
+    "destination": "/posts/read-locally-txt-file-use-fetch-method-in-javascript",
+    "permanent": true
+  },
+  {
+    "source": "/setting-newly-launch-ghost-cms-on-google-cloud-platform",
+    "destination": "/posts/setting-newly-launch-ghost-cms-on-google-cloud-platform",
+    "permanent": true
+  },
+  {
+    "source": "/six-trusted-places-where-you-buy-linux-laptops",
+    "destination": "/posts/six-trusted-places-where-you-buy-linux-laptops",
+    "permanent": true
+  },
+  {
+    "source": "/test-internet-speed-in-ubuntu-and-raspberry-pi-4-with-cli-tool",
+    "destination": "/posts/test-internet-speed-in-ubuntu-and-raspberry-pi-4-with-cli-tool",
+    "permanent": true
+  },
+  {
+    "source": "/text-highlighting-in-html-5",
+    "destination": "/posts/text-highlighting-in-html-5",
+    "permanent": true
+  },
+  {
+    "source": "/the-5-best-websites-to-learn-javascript-for-free",
+    "destination": "/posts/the-5-best-websites-to-learn-javascript-for-free",
+    "permanent": true
+  },
+  {
+    "source": "/the-best-5-ide-for-raspberry-pi-4-in-2022",
+    "destination": "/posts/the-best-5-ide-for-raspberry-pi-4-in-2022",
+    "permanent": true
+  },
+  {
+    "source": "/the-simple-way-to-use-global-sass-in-side-next-js",
+    "destination": "/posts/the-simple-way-to-use-global-sass-in-side-next-js",
+    "permanent": true
+  },
+  {
+    "source": "/title-tag-in-html-5",
+    "destination": "/posts/title-tag-in-html-5",
+    "permanent": true
+  },
+  {
+    "source": "/top-3-ways-convert-your-programming-code-into-a-beautiful-image",
+    "destination": "/posts/top-3-ways-convert-your-programming-code-into-a-beautiful-image",
+    "permanent": true
+  },
+  {
+    "source": "/what-are-html-elements",
+    "destination": "/posts/what-are-html-elements",
+    "permanent": true
+  },
+  {
+    "source": "/what-are-html-eteribute",
+    "destination": "/posts/what-are-html-eteribute",
+    "permanent": true
+  },
+  {
+    "source": "/what-are-html-tags",
+    "destination": "/posts/what-are-html-tags",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-css-flax-box",
+    "destination": "/posts/what-is-css-flax-box",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-html",
+    "destination": "/posts/what-is-html",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-man-command-in-linux",
+    "destination": "/posts/what-is-man-command-in-linux",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-mdbook-and-how-to-install-mdbook-cli",
+    "destination": "/posts/what-is-mdbook-and-how-to-install-mdbook-cli",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-next-js",
+    "destination": "/posts/what-is-next-js",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-npm-init-command-and-some-of-the-best-practices",
+    "destination": "/posts/what-is-npm-init-command-and-some-of-the-best-practices",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-npm-install-command",
+    "destination": "/posts/what-is-npm-install-command",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-stiky-note-and-how-to-increate-productive-as-linux-user",
+    "destination": "/posts/what-is-stiky-note-and-how-to-increate-productive-as-linux-user",
+    "permanent": true
+  },
+  {
+    "source": "/what-is-the-linux-tree-command-and-how-is-it-used",
+    "destination": "/posts/what-is-the-linux-tree-command-and-how-is-it-used",
+    "permanent": true
+  },
+  {
+    "source": "/what-limitations-to-using-tauri-and-nextjs-together-and-frontend-developers-build-an-application-without-knowledge-of-rust-language",
+    "destination": "/posts/what-limitations-to-using-tauri-and-nextjs-together-and-frontend-developers-build-an-application-without-knowledge-of-rust-language",
+    "permanent": true
+  },
+  {
+    "source": "/why-i-switched-to-neovim-from-vscode-and-how-did-you-configure-neovim",
+    "destination": "/posts/why-i-switched-to-neovim-from-vscode-and-how-did-you-configure-neovim",
+    "permanent": true
+  }
+
+]
+
+module.exports= redirectUrl
+
+// export default redirectUrl
