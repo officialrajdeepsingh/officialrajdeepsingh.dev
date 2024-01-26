76920e9ae0e8a8b0511782a6fbe6a56406955c03
Rajdeep Singh <radewala.2@gmail.com>
2024-01-26 21:01:27 +0530

    tweet component

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,12 @@
+import { Tweet } from "react-tweet";
+
+export function XTweet() {
+  return (
+    <div className=" nx-not-format grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
+      <Tweet id="1718296988729356404" />
+      <Tweet id="1647193298908397571" />
+      <Tweet id="1633770889119506432" />
+      <Tweet id="1581469371322445825" />
+    </div>
+  );
+}
